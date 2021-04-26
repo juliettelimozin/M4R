48de06f124df261705cc0a66f8db930ecfc3e1f0
Juliette Limozin <juliette.limozin17@imperial.ac.uk>
2021-04-26 11:07:20 +0100

    RF moreW npy and figures

--- Code/SVM_DR.py
+++ Code/SVM_DR.py
@@ -1,32 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 11:33:28 2021
+
+@author: juliette
+"""
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import expit
-from sklearn.linear_model import LogisticRegression
 random.seed(1234)
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-from DR_estimation import DR_estimator, RF_crossval
-import time
 W1 = np.random.uniform(-2,2,1000000)
 W2 = np.random.binomial(1,0.5,1000000)
-W3 = np.random.normal(0,1,1000000)
-W4 = np.random.exponential(1,1000000)
-A = np.random.binomial(1, expit(-W1 + 2*np.multiply(W1,W2) - W3 + 2*np.multiply(W3,W4)), 1000000)
-Y = np.random.binomial(1, expit(0.2*A-W1 + 2*np.multiply(W1,W2) - W3 + 2*np.multiply(W3,W4)), 1000000)
+A = np.random.binomial(1, expit(-W1 + 2*np.multiply(W1,W2)), 1000000)
+Y =np.random.binomial(1, expit(0.2*A-W1 + 2*np.multiply(W1,W2)), 1000000)
 
-B_true = np.mean(expit(0.2-W1 + 2*np.multiply(W1,W2) - W3 + 2*np.multiply(W3,W4)))
+B_true = np.mean(expit(0.2-W1+ 2*np.multiply(W1,W2)))
+
 iters = 2500
-N = [200*i for i in range(1,7)]
+N = [200*i for i in range(1,6)]
 
 estimates = np.zeros((9,iters,len(N)))
 CI = 1.0*estimates
 SEs = 1.0*CI
 
-for i in range(len(N)):
-    estimates[:,:,i] = np.load('estimates_RF_moreW_'+ str(N[i]) + '.npy')
-    CI[:,:,i] = np.load('CI_RF_moreW_'+ str(N[i]) + '.npy')
-    SEs[:,:,i] = np.load('SEs_RF_moreW_'+ str(N[i]) + '.npy')
+for i in range(5):
+    estimates[:,:,i] = np.load('estimates_SVM_'+ str(N[i]) + '.npy')
+    CI[:,:,i] = np.load('CI_SVM_'+ str(N[i]) + '.npy')
+    SEs[:,:,i] = np.load('SEs_SVM_'+ str(N[i]) + '.npy')
    
 bias = np.zeros((9,len(N)))       
 for i in range(9):
@@ -41,14 +44,14 @@ for i in range(9):
                                                              axis = 0)
 
 
-labels = ['DR logistic ps, forest om',
-          'DR logistic om, forest ps',
-          'DR forest',
-          'DR wrong logistic om, forest ps',
-          'DR wrong loigstic ps, forest om',
-          'DR wrong forest om, forest ps',
-          'DR wrong forest ps, forest om',
-          'DR wrong forest ps & om',
+labels = ['DR logistic ps, SVM om',
+          'DR logistic om, SVM ps',
+          'DR SVM',
+          'DR wrong logistic om, SVM ps',
+          'DR wrong loigstic ps, SVM om',
+          'DR wrong SVM om, SVM ps',
+          'DR wrong SVM ps, SVM om',
+          'DR wrong SVM ps & om',
           'DR exact']
 
 plt.figure(figsize = (10,6.6))
@@ -59,7 +62,7 @@ plt.hlines(0, N[0], N[-1], linestyles = 'dashed', label = '95% goal')
 plt.xlabel('Sample size', fontsize = 15)
 plt.ylabel('Bias', fontsize = 15)
 plt.title('Emperial bias convergence of various DR estimators', fontsize = 15)
-plt.savefig('../figures/biasRF_moreW.png')
+plt.savefig('../figures/biasRF.png')
 plt.show()
 
 plt.figure(figsize = (10,6.6))
@@ -69,7 +72,7 @@ for i in range(9):
 plt.hlines(0.95, N[0], N[-1], linestyles = 'dashed', label = '95% goal')
 plt.xlabel('Sample size', fontsize =15)
 plt.ylabel('Coverage', fontsize = 15)
-plt.savefig('../figures/CIRF_moreW.png')
+plt.savefig('../figures/CIRF.png')
 plt.legend()
 plt.show()
 
@@ -81,7 +84,7 @@ plt.hlines(1, N[0], N[-1], linestyles = 'dashed', label = '95% goal')
 plt.xlabel('Sample size', fontsize = 15)
 plt.ylabel(r'SD($ \hat \beta )/ \widebar{\widehat{se}_{n}}$', fontsize = 15)
 plt.title('Ratio of standard deviation to mean standard error estimates', fontsize = 15)
-plt.savefig('../figures/SERF_moreW.png')
+plt.savefig('../figures/SERF.png')
 plt.show()
 
 plt.figure(figsize = (10,6.6))
@@ -92,7 +95,7 @@ plt.hlines(0, N[0], N[-1], linestyles = 'dashed', label = '95% goal')
 plt.xlabel('Sample size', fontsize = 15)
 plt.ylabel('$\sqrt{N}$Bias', fontsize = 15)
 plt.title('$\sqrt{N}$bias convergence of various DR estimators', fontsize = 15)
-plt.savefig('../figures/sqrtnRF_moreW.png')
+plt.savefig('../figures/sqrtnRF.png')
 plt.show()
 
 
@@ -131,5 +134,5 @@ ax[1,0].vlines(B_true, 0, 120,label = 'True mean')
 
 fig.legend()
 fig.suptitle(r'Histograms of estimates for sample size $N = 1000$', fontsize = 15)
-plt.savefig('../figures/histRF_moreW.png')
+plt.savefig('../figures/histRF.png')
 plt.show()
