f6a565dfeb6c2f09f7edb78648de6bb5a03486f6
Juliette Limozin <juliette.limozin17@imperial.ac.uk>
2021-06-07 15:59:38 +0100

    Modifying DR estimation code to good practice

--- Code/DR_estimation.py
+++ Code/DR_estimation.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 13 12:11:29 2021
-
-@author: juliette
-"""
 import random
 import numpy as np
 from sklearn.linear_model import LogisticRegression
@@ -20,13 +15,24 @@ from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 
 def RF_crossval(Xtrain, ytrain, Xtest,):
+    '''
+    Cross-validation and fitting for Random Forest.
+    Input: 
+        - Xtrain: training feature
+        - ytrain: training outcome 
+        - Xtest: testing/fitting feature
+    Output:
+        output: trained RF with hyperparameter chosen from cross-valdiation 
+                output
+    '''
     skf = StratifiedKFold(n_splits=5, shuffle = True, random_state = 999)
     
     #Fit it to the train set
     cv=skf.split(Xtrain, ytrain)
     
     # Choose the values of the number of decision trees, their depth and 
-    # the maximum number of descriptors (features) randomly chosen at each split that I want to test out with GridSearchCV
+    # the maximum number of descriptors (features) randomly chosen at each 
+    #split that I want to test out with GridSearchCV
     n_estimators = np.arange(600,1001,200)
     max_depth = np.arange(10,31,5)
     max_features = np.arange(1,int(Xtrain.shape[1])+1,1)
@@ -35,9 +41,11 @@ def RF_crossval(Xtrain, ytrain, Xtest,):
     hyperF = dict(n_estimators = n_estimators, max_depth = max_depth,  
                   max_features = max_features)
     
-    # Set GridSearch CV with model to tune as RFC, grid of hyperparameters, cross validation method as
-    # stratified K fold, and the scoring method as the  the accuracy of your validation predictions
-    gridF = GridSearchCV(RandomForestClassifier(), hyperF, cv = cv, scoring = 'accuracy', verbose = 1, 
+    # Set GridSearch CV with model to tune as RF, grid of hyperparameters, 
+    #cross validation method as stratified K fold, and the scoring method as 
+    #the  the accuracy of your validation predictions
+    gridF = GridSearchCV(RandomForestClassifier(), hyperF, cv = cv, 
+                         scoring = 'accuracy', verbose = 1, 
                           n_jobs = -1)
     gridF.fit(Xtrain, ytrain)
     print(gridF.best_params_)
@@ -46,7 +54,50 @@ def RF_crossval(Xtrain, ytrain, Xtest,):
     output[np.logical_and(0> output, output>-0.000001)] = -0.000001
     return output
 
+
+def MLP_crossval(Xtrain, ytrain, Xtest,):
+    '''
+    Cross-validation and fitting for MLP.
+    Input: 
+        - Xtrain: training feature
+        - ytrain: training outcome 
+        - Xtest: testing/fitting feature
+    Output:
+        output: trained MLP with hyperparameter chosen from cross-valdiation
+                output
+    '''
+    skf = StratifiedKFold(n_splits=5, shuffle = True, random_state = 999)
+    
+    #Fit it to the train set
+    cv=skf.split(Xtrain, ytrain)
+    tuned_params = {'hidden_layer_sizes': [(10,20,30), (50,50), (100,)],
+                    'learning_rate':['constant','invscaling', 'adaptive'],
+                    'activation': ["logistic", "relu", "Tanh"],
+                    'alpha': [10.0 **(-i) for i in range(1,5)]
+                    }
+    # Set GridSearch CV with model to tune as MLP, grid of hyperparameters, 
+    #cross validation method as stratified K fold, and the scoring method as 
+    #the  the accuracy of your validation predictions
+    gridMLP = GridSearchCV(MLPClassifier(solver = 'sgd', max_iter = 500), 
+                           tuned_params, cv = cv, 
+                           scoring='accuracy', verbose = 1, n_jobs = -1)
+    gridMLP.fit(Xtrain, ytrain)
+    output =  gridMLP.predict_proba(Xtest)[:,1]
+    output[np.logical_and(0<= output, output<0.000001)] = 0.000001
+    output[np.logical_and(0> output, output>-0.000001)] = -0.000001
+    return output
+
 def SVM_crossval(Xtrain, ytrain, Xtest,):
+    '''
+    Cross-validation and fitting for SVM.
+    Input: 
+        - Xtrain: training feature
+        - ytrain: training outcome 
+        - Xtest: testing/fitting feature
+    Output:
+        output: trained SVM with hyperparameter chosen from cross-valdiation 
+                output
+    '''
     skf = StratifiedKFold(n_splits=5, shuffle = True, random_state = 999)
     
     #Fit it to the train set
@@ -59,8 +110,9 @@ def SVM_crossval(Xtrain, ytrain, Xtest,):
                      'gamma': [1e-3, 1e-4, 1e-5], 
                      'degree': np.arange(1,int(Xtrain.shape[1])+1,1), 
                      'C': [10, 100, 1000]}]
-    # Set GridSearch CV with model to tune as RFC, grid of hyperparameters, cross validation method as
-    # stratified K fold, and the scoring method as the  the accuracy of your validation predictions
+    # Set GridSearch CV with model to tune as SVM, grid of hyperparameters, 
+    #cross validation method as stratified K fold, and the scoring method as 
+    #the  the accuracy of your validation predictions
     gridSVM = GridSearchCV(SVC(probability = True), tuned_params, cv = cv, 
                            scoring='accuracy', verbose = 1, n_jobs = -1)
     gridSVM.fit(Xtrain, ytrain)
@@ -69,29 +121,10 @@ def SVM_crossval(Xtrain, ytrain, Xtest,):
     output[np.logical_and(0> output, output>-0.000001)] = -0.000001
     return output
 
-
-def MLP_crossval(Xtrain, ytrain, Xtest,):
-    skf = StratifiedKFold(n_splits=5, shuffle = True, random_state = 999)
-    
-    #Fit it to the train set
-    cv=skf.split(Xtrain, ytrain)
-    tuned_params = {'hidden_layer_sizes': [(10,20,30), (50,50), (100,)],
-                    'learning_rate':['constant','invscaling', 'adaptive'],
-                    'activation': ["logistic", "relu", "Tanh"],
-                    'alpha': [10.0 **(-i) for i in range(1,5)]
-                    }
-    # Set GridSearch CV with model to tune as RFC, grid of hyperparameters, cross validation method as
-    # stratified K fold, and the scoring method as the  the accuracy of your validation predictions
-    gridMLP = GridSearchCV(MLPClassifier(solver = 'sgd', max_iter = 500), 
-                           tuned_params, cv = cv, 
-                           scoring='accuracy', verbose = 1, n_jobs = -1)
-    gridMLP.fit(Xtrain, ytrain)
-    output =  gridMLP.predict_proba(Xtest)[:,1]
-    output[np.logical_and(0<= output, output<0.000001)] = 0.000001
-    output[np.logical_and(0> output, output>-0.000001)] = -0.000001
-    return output
-
 class DR_estimator:
+    ''''
+    Class for DR estimation procedure
+    '''
     def __init__(self, Y, W, A):
         self.Y = Y
         self.W = W
@@ -101,7 +134,12 @@ class DR_estimator:
     def fit_regression(self, ps = None, om = None, premade = True,
                        method_ps = 'Logistic',
                        method_om = 'Logistic'):
-                       
+        '''
+        Fitting nuisance parameter models; option to add pre-made models, 
+        or construct Logistic, RF or MLP models.
+        
+        Output: self with nuisance parameter models specified
+        '''
         if premade:
             try:
                 self.ps = ps
@@ -110,31 +148,45 @@ class DR_estimator:
                 print('Please define regression models')
         else:
             if method_ps == 'Logistic':
-                self.ps = LogisticRegression().fit(self.W,self.A).predict_proba(self.W)[:,1]
+                self.ps = LogisticRegression().fit(self.W,self.A) \
+                            .predict_proba(self.W)[:,1]
             elif method_ps == 'Random Forest':
                 X_train, _, y_train, _ = train_test_split(self.W,self.A)
                 self.ps = RF_crossval(X_train,y_train,self.W)
-            elif method_ps == 'SVM':
+            elif method_ps == 'MLP':
                 X_train, _, y_train, _ = train_test_split(self.W,self.A)
-                self.ps = SVM_crossval(X_train,y_train,self.W)
+                self.ps = MLP_crossval(X_train,y_train,self.W)
             else: 
                 raise ValueError("No regression method")
 
             if method_om == 'Logistic':
-                self.om = LogisticRegression().fit(self.W[self.A==1],self.Y[self.A==1]).predict_proba(self.W)[:,1]
+                self.om = LogisticRegression().fit(self.W[self.A==1],
+                                                   self.Y[self.A==1])\
+                                        .predict_proba(self.W)[:,1]
             elif method_om == 'Random Forest':
-                X_train, _, y_train, _ = train_test_split(self.W[self.A==1],self.Y[self.A==1])
+                X_train, _, y_train, _ = train_test_split(self.W[self.A==1],
+                                                          self.Y[self.A==1])
                 self.om = RF_crossval(X_train,y_train,self.W)
-            elif method_om == 'SVM':
-                X_train, _, y_train, _ = train_test_split(self.W[self.A==1],self.Y[self.A==1])
-                self.om = SVM_crossval(X_train,y_train,self.W)
+            elif method_om == 'MLP':
+                X_train, _, y_train, _ = train_test_split(self.W[self.A==1],
+                                                          self.Y[self.A==1])
+                self.om = MLP_crossval(X_train,y_train,self.W)
             else: 
                 raise ValueError("No regression method")
         return self
         
     
     def estimate(self):
+        '''
+        Construc DR and Lunceford-Davidian SE estimate based on class 
+        nuisance parameter models
+        
+        Output: tuple of DR and SE estimate based on self's nuisance parameter
+                models
+        '''
         DR = np.mean(self.A*self.Y*(1/self.ps)
                    + (np.ones(self.size)-self.A*(1/self.ps))*self.om)
-        se = np.sqrt(np.sum(np.square(self.A*(self.Y-self.om)*(1/self.ps)+self.om - DR*np.ones(self.size))))/self.size
+        se = np.sqrt(np.sum(np.square(self.A*(self.Y-self.om)*(1/self.ps)
+                                      +self.om 
+                                      - DR*np.ones(self.size))))/self.size
         return DR, se 
